ef31e45109e3be868a2091394e635903d02a2c20
Steffen Hyllested Pedersen <111784299+SteffenHyllested@users.noreply.github.com>
2024-09-27 05:21:57 +0200

    Removed redundant font data Font names are now properly namespaced

--- lua/entities/atm/cl_init.lua
+++ lua/entities/atm/cl_init.lua
@@ -1,10 +1,10 @@
 include("shared.lua")
 local imgui = include("libs/imgui.lua")
 
-local MainFontData = {font = "Arial",size = 36,weight = 1000,blursize = 0,scanlines = 0,antialias = true,underline = false,italic = false,strikeout = false,symbol = false,rotary = false,shadow = false,additive = false,outline = false,}
-local SmallMainFontData = {font = "Arial",size = 20,weight = 1500,blursize = 0,scanlines = 0,antialias = true,underline = false,italic = false,strikeout = false,symbol = false,rotary = false,shadow = false,additive = false,outline = false,}
-surface.CreateFont("MainFont", MainFontData)
-surface.CreateFont("SmallMainFont", SmallMainFontData)
+local MainFontData = {font = "Arial",size = 36,weight = 1000,}
+local SmallMainFontData = {font = "Arial",size = 20,weight = 1500,}
+surface.CreateFont("HyllestedMoney:MainFont", MainFontData)
+surface.CreateFont("HyllestedMoney:MainFontSmall", SmallMainFontData)
 
 function ENT:Initialize()
     self.Increment = 1
@@ -17,28 +17,28 @@ function ENT:DrawTranslucent()
             surface.SetDrawColor(50,50,50,255)
             surface.DrawRect(0,0,400,230)
 
-            draw.DrawText("ATM - MoneyBank™", "MainFont",200,20, Color( 255,255,255, 255 ),TEXT_ALIGN_CENTER)
-            draw.DrawText("Current Balance:", "SmallMainFont",52,66, Color( 255,255,255, 255 ),TEXT_ALIGN_LEFT)
-            draw.DrawText(string.format("$%.2f",client:GetNWInt("BankMoney")), "MainFont",348,56, Color( 100,255,150, 255 ),TEXT_ALIGN_RIGHT)
+            draw.DrawText("ATM - MoneyBank™", "HyllestedMoney:MainFont",200,20, Color( 255,255,255, 255 ),TEXT_ALIGN_CENTER)
+            draw.DrawText("Current Balance:", "HyllestedMoney:MainFontSmall",52,66, Color( 255,255,255, 255 ),TEXT_ALIGN_LEFT)
+            draw.DrawText(string.format("$%.2f",client:GetNWInt("BankMoney")), "HyllestedMoney:MainFont",348,56, Color( 100,255,150, 255 ),TEXT_ALIGN_RIGHT)
 
             surface.SetDrawColor(35,35,35,255)
             surface.DrawRect(52,100,30,50)
             surface.DrawRect(318,100,30,50)
 
-            draw.DrawText("<", "MainFont", 58, 108, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT)
-            draw.DrawText(">", "MainFont", 342, 108, Color( 255, 255, 255, 255 ), TEXT_ALIGN_RIGHT)
+            draw.DrawText("<", "HyllestedMoney:MainFont", 58, 108, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT)
+            draw.DrawText(">", "HyllestedMoney:MainFont", 342, 108, Color( 255, 255, 255, 255 ), TEXT_ALIGN_RIGHT)
 
             surface.SetDrawColor(25,25,25,255)
             surface.DrawRect(82,100,236,50)
 
-            draw.DrawText(string.format("$%.2f",self.Increment),"MainFont",200,108,Color(255,255,255,255), TEXT_ALIGN_CENTER)
+            draw.DrawText(string.format("$%.2f",self.Increment),"HyllestedMoney:MainFont",200,108,Color(255,255,255,255), TEXT_ALIGN_CENTER)
 
             surface.SetDrawColor(20,120,60,255)
             surface.DrawRect(52,160,144,50)
             surface.DrawRect(204,160,144,50)
 
-            draw.DrawText("Deposit","SmallMainFont",124,175,Color(255,255,255,255), TEXT_ALIGN_CENTER)
-            draw.DrawText("Withdraw","SmallMainFont",276,175,Color(255,255,255,255), TEXT_ALIGN_CENTER)
+            draw.DrawText("Deposit","HyllestedMoney:MainFontSmall",124,175,Color(255,255,255,255), TEXT_ALIGN_CENTER)
+            draw.DrawText("Withdraw","HyllestedMoney:MainFontSmall",276,175,Color(255,255,255,255), TEXT_ALIGN_CENTER)
 
             if imgui.IsPressed() then
                 local transferRate = 0
