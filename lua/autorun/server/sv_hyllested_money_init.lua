9eb651c1b9762236ff2b560f0042f2a75c407661
Steffen Hyllested Pedersen <111784299+SteffenHyllested@users.noreply.github.com>
2024-09-27 06:29:52 +0200

    Switched from JSON datasaving to an SQL database

--- lua/autorun/server/sv_hyllested_money_init.lua
+++ lua/autorun/server/sv_hyllested_money_init.lua
@@ -1,34 +1,41 @@
 AddCSLuaFile("autorun/client/cl_hyllested_money_init.lua")
 
-local DATA_FILE = "moneySaveData.txt"
+local TABLE_NAME = "HyllestedMoney:PlayerData" // Note there are quotes inside the string as the table name includes a colon
 
-function LoadDataFromFile()
-	-- Create the data file if it doesn't exist
-	if not file.Exists(DATA_FILE, "DATA") then
-		file.Write(DATA_FILE, "")
+-- Create the table if it doesn't exist
+if not sql.TableExists(TABLE_NAME) then
+	sql.Begin()
+		sql.Query("CREATE TABLE `" .. TABLE_NAME .. "`( id int , bank int, wallet int )")
+	sql.Commit()
+end
+
+function SavePlayerData( client )
+	local bank, wallet = client:GetNWInt("BankMoney"), client:GetNWInt("Money")
+	local query = string.format("UPDATE `%s` SET bank=%d, wallet=%d WHERE id=%q", TABLE_NAME, bank, wallet, client:SteamID64() )
+	sql.Begin()
+		sql.Query(query)
+	sql.Commit()
+end
+
+function LoadPlayerData( client )
+	local query = string.format("SELECT bank, wallet FROM `%s` WHERE id=%q", TABLE_NAME, client:SteamID64() )
+	local playerData = sql.QueryRow("SELECT bank, wallet FROM `" .. TABLE_NAME .. "` WHERE id=" .. client:SteamID64() ) // This structuring could be prone to SQL injection, however given the nature of the values being concatenated, it is safe here.
+
+	if not playerData then
+		sql.Begin()
+			local query = string.format("INSERT INTO `%s` ( id, bank, wallet ) VALUES ( %q, %d, %d )", TABLE_NAME, client:SteamID64(), 0, 100 )
+			sql.Query( query )
+		sql.Commit()
+		client:SetNWInt( "Money", 100 )
+		client:SetNWInt( "BankMoney", 0 )
+	else
+		client:SetNWInt( "Money", tonumber(playerData.wallet) )
+		client:SetNWInt( "BankMoney", tonumber(playerData.bank) )
 	end
-
-	local content = file.Read(DATA_FILE, "DATA") -- read saved data
-	return util.JSONToTable(content or "") or {} -- json decode it, default to empty table
 end
-
-function SaveDataToFile(data)
-	local content = util.TableToJSON(data) -- json encode it
-	file.Write(DATA_FILE, content) -- write to file
-end
-
-local playerData = LoadDataFromFile()
 
 hook.Add( "PlayerInitialSpawn","moneyaddon-on-join",function(client)
-	if playerData[client:AccountID()] then -- if the player has data, load it
-		client:SetNWInt("BankMoney",playerData[client:AccountID()].Bank)
-		client:SetNWInt("Money",playerData[client:AccountID()].Wallet)
-	else -- otherwise create and save data for them
-		client:SetNWInt("Money",100)
-		client:SetNWInt("BankMoney",0)
-		playerData[client:AccountID()] = {Bank = 0, Wallet = 100}
-		SaveDataToFile(playerData)
-	end
+	LoadPlayerData(client)
 end)
 
 util.AddNetworkString("HyllestedMoney:TransferMoney")
@@ -40,6 +47,9 @@ net.Receive("HyllestedMoney:TransferMoney",function(length,client)
 	local moneyReq = transferRate*transferAmount
 	local bankReq = -transferRate*transferAmount
 
+	print(moneyReq)
+	print(client:GetNWInt("Money"))
+
 	if moneyReq > client:GetNWInt("Money") then return end -- Not enough money in wallet
 	if bankReq > client:GetNWInt("BankMoney") then return end -- Not enough money in bank
 	-- We check both so I don't have to branch off depending on whether it's a deposit or withdrawal
@@ -49,6 +59,5 @@ net.Receive("HyllestedMoney:TransferMoney",function(length,client)
 	-- viola
 
 	-- Update save file
-	playerData[client:AccountID()] = {Bank = client:GetNWInt("BankMoney"), Wallet = client:GetNWInt("Money")}
-	SaveDataToFile(playerData)
+	SavePlayerData(client)
 end)
