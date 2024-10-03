54c493ad1de490e7405e5e163afcbadfdd0bf552
Steffen Hyllested Pedersen <111784299+SteffenHyllested@users.noreply.github.com>
2024-10-03 20:02:29 +0200

    Fixed error in regards to wrong font identifier being used.

--- lua/autorun/client/cl_hyllested_money_init.lua
+++ lua/autorun/client/cl_hyllested_money_init.lua
@@ -8,6 +8,6 @@ hook.Add("HUDPaint", "draw-money-hud", function()
     local Width, Height = ScrW(), ScrH()
     local BoxWidth, BoxHeight = 300, 100
     draw.RoundedBox(15, Width/2 - BoxWidth/2, 50, BoxWidth, BoxHeight, Color(0,0,0,100))
-    draw.SimpleTextOutlined("Current Balance", "MainFont", Width/2, 55, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 2, Color(0,0,0,255))
-    draw.SimpleTextOutlined(string.format("$%.2f",client:GetNWInt("Money")), "HyllstedMoney:MainFont", Width/2, 100, Color( 100, 255, 100, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 2, Color(0,0,0,255))
+    draw.SimpleTextOutlined("Current Balance", "HyllestedMoney:MainFont", Width/2, 55, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 2, Color(0,0,0,255))
+    draw.SimpleTextOutlined(string.format("$%.2f",client:GetNWInt("Money")), "HyllestedMoney:MainFont", Width/2, 100, Color( 100, 255, 100, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 2, Color(0,0,0,255))
 end)
