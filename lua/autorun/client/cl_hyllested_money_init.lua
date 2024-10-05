e47d7a8363c29cd853331efa80dd0b068eb7e501
Steffen Hyllested Pedersen <111784299+SteffenHyllested@users.noreply.github.com>
2024-10-05 02:26:17 +0200

    Fixed issue regarding fonts no longer being properly created.

---
+++ lua/autorun/client/cl_hyllested_money_init.lua
@@ -0,0 +1,4 @@
+local MainFontData = {font = "Arial",size = 36,weight = 1000,}
+local SmallMainFontData = {font = "Arial",size = 20,weight = 1500,}
+surface.CreateFont("HyllestedMoney:MainFont", MainFontData)
+surface.CreateFont("HyllestedMoney:MainFontSmall", SmallMainFontData)
